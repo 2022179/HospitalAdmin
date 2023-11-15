365ef9772b6ffb76a80c33d345d511f016ecb40a
2022179 <146199008+2022179@users.noreply.github.com>
2023-11-15 13:39:16 +0000

    fixed a few errors on databasesetup file
    had to remove the single colons
    line 31 on database setup was  wrong
    removed last bracket

--- src/hospitaladmin/DatabaseSetup.java
+++ src/hospitaladmin/DatabaseSetup.java
@@ -14,27 +14,28 @@ import java.sql.Statement;
  * @author Primm
  */
 public class DatabaseSetup {
-   final String DB_BASE_URL = "jdbc:mysql://localhost";
+   final static String DB_BASE_URL = "jdbc:mysql://localhost";
     //jdbc=java database connector
-    final String USER ="ooc2023";
-    final String PASSWORD = "OOC2023";
+    final static String USER ="ooc2023";
+    final static String PASSWORD = "ooc2023";
     // THIS WILL ALWAY SHOW WHEN CONNECTING DATABASE 
-    public boolean setupDB() throws SQLException{
+    public static boolean setupDB() throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException{
+        Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
         try(
             Connection conn= DriverManager.getConnection( DB_BASE_URL,USER,PASSWORD);
             Statement stmt = conn.createStatement();
             
             ){
-                stmt.execute("CREATE DATABASE IF NOT EXIST'hospital';");
-                stmt.execute("USER 'hospital';");
-                String sql=
-                        "CREATE DATABASE IF NOT EXIST'patientData'("
-                        +"'name ' VARCHAR(255),"
-                        +"birthdate'DATE'"
-                        +"'bloodtype ' VARCHAR(2),"
-                        +"'id' INT(10)"
-                        //the number are to specify how long it should be
-                        ");";
+                stmt.execute("CREATE DATABASE IF NOT EXISTS hospital;");
+                stmt.execute("USE hospital;");
+                String sql=//the number are to specify how long it should be
+                        "CREATE TABLE IF NOT EXISTS patientData("
+                        + "name VARCHAR(255),"
+                        + "birthdate DATE,"
+                        + "bloodtype VARCHAR(2),"
+                        + "id INT(10)"
+                        + ");";
+                
                 stmt.execute(sql);
                 return true;
         }catch(Exception e){
@@ -43,5 +44,4 @@ public class DatabaseSetup {
                 
             }
     }
-}
 }
--- src/hospitaladmin/HospitalAdmin.java
+++ src/hospitaladmin/HospitalAdmin.java
@@ -4,6 +4,8 @@
  */
 package hospitaladmin;
 
+import java.sql.SQLException;
+
 /**
  *
  * @author Primm
@@ -13,7 +15,7 @@ public class HospitalAdmin {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
         patient p1= new patient("Oglobe","25/03/2023","A+");
         patient p2= new patient("Vernon","26/03/2023","O+");
         // to get patient ID 
@@ -22,6 +24,12 @@ public class HospitalAdmin {
         // TODO code application logic here
         
         System.out.println(patient.getCurrentID());
+        if (DatabaseSetup.setupDB()){
+            System.out.println("Data table created, you are a STAR Primmy");
+            
+        }else{
+            System.out.println("ohh no, something is wrong with your database");
+        }
     }
     
 }
